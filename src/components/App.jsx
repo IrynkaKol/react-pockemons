175c6e354ead8d21d64eec2f64a0bc1ad18ce354
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-20 22:58:33 +0100

    add pokemon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+//import { ToastContainer } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
 import PokemonForm from './PokemonForm/PokemonForm';
+import PokemonInfo from './PokemonInfo/PokemonInfo'
 
 export class App extends Component {
   state = {
@@ -14,7 +16,8 @@ export class App extends Component {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
         <PokemonForm onSubmit={this.handleFormSubmit} />
-        <ToastContainer autoClose={3000}/>
+        <PokemonInfo pokemonName={this.state.pokemonName}/>
+        <Toaster autoClose={3000}/>
       </div>
     );
   }
--- src/components/PokemonInfo/PokemonInfo.jsx
+++ src/components/PokemonInfo/PokemonInfo.jsx
@@ -1,8 +1,59 @@
-export default class PokemonInfo extends Comment {
-    render () {
-        return 
-            <div>Pokemon Info</div>
-        
-    }
+import { Component } from 'react';
 
+export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    loading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+    if (prevName !== nextName) {
+      console.log('Змінилось Name покемона');
+      //console.log('prevProps.pokemonInfo', prevProps.pokemonName)
+      //console.log('this.props.pokemonInfo', this.props.pokemonName)
+      this.setState({ loading: true, pokemon: null });
+      fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error(`Покемона з name ${nextName} немає`));
+        })
+        .then(pokemon => this.setState({ pokemon }))
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+
+  render() {
+    const { pokemon, loading, error } = this.state;
+    const { pokemonName } = this.props;
+    return (
+      <div>
+        {error && <h1>{error.message}</h1>}
+        {loading && <div>Завантажуємо ...</div>}
+        {!pokemonName && <div>Введить name покемона</div>}
+        {pokemon && (
+          <div>
+            <p>{pokemon.name}</p>
+            <img
+              src={pokemon.sprites.other['official-artwork'].front_default}
+              alt={pokemon.name}
+              width="240"
+            />
+            <ul>
+              {pokemon.stats.map(entry => (
+                <li key={entry.stat.name}>
+                  {entry.stat.name}: {entry.base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
